943563fb76639188a88e0a80caf9bf7400ff7733
Naagii666 <naagii.dashdorj@gmail.com>
2019-05-24 14:32:22 +0800

    naagii
    help

--- App/TLogin/Dashboard/help.js
+++ App/TLogin/Dashboard/help.js
@@ -15,26 +15,18 @@ class help extends Component {
     
   render () {
     return (
-      <View style={{justifyContent: 'center', alignItems:'center',flex:1,marginTop:'10%'}}>
+      <View style={{ alignItems:'center',flex:1}}>
         <View>
-          <Text>АШИГЛАХ ЗААВАР</Text>
-      </View>
-        <View>
+          <Text style={{ marginVertical:'5%'}}>АШИГЛАХ ЗААВАР</Text>
+        </View>
+        <View style={{height:200}}>
           <WebView
-<<<<<<< HEAD
-            style={{ height:300, width:290 }}
-            // style={{ height:300, width:290 }}
-=======
-            // style={{flex:1}}
-            style={{ height:100, width:290 }}
->>>>>>> 5d6eb833c6eec6371936d96b8bd9872469aa7246
+            style={{width:290}}
             javaScriptEnabled={true}
-            // source={{uri: 'https://www.youtube.com/embed/03DskGP1Ct0'}}
-            source={{ html: "<html><body><iframe src='https://www.youtube.com/embed/03DskGP1Ct0' frameborder='0' allowfullscreen></iframe></body></html>" }}
             automaticallyAdjustContentInsets={false}
+            source={{uri: 'https://www.youtube.com/embed/03DskGP1Ct0'}}
           />
         </View>
-
       </View>
       
     );
