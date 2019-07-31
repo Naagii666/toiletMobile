bbd2dc9d880d4a6c0dff60237aa2985cd6fc352d
Naagii666 <naagii.dashdorj@gmail.com>
2019-07-31 15:55:28 +0800

    v1.12
    v1.12

--- App/TLogin/product/DetialFaCheck.js
+++ App/TLogin/product/DetialFaCheck.js
@@ -23,7 +23,7 @@ class DetialFaCheck extends Component{
     const otherParam = navigation.getParam('price', 'NO-ID');
     const definition = navigation.getParam('definition', 'some default value');
     const products_image = navigation.getParam('url', 'some default value');
-    const regex = /([&]nbsp[;])*(<.*?>)/ig;
+    const regex = /([&].*?[;])*(<.*?>)/ig;
     return(
       <ScrollView>
         <View style={styles.container}>
